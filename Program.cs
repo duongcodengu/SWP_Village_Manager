72d8c452e8d8e7c073e8eb5a22e0fcae28171450
bao30012005 <quocbao30012005@gmail.com>
2025-06-18 10:40:26 +0700

    Muahang

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer("vllage_manager_database")
+           .EnableSensitiveDataLogging()
+           .LogTo(Console.WriteLine, LogLevel.Information));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 // Add session support
@@ -35,3 +38,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
