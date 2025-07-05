ea8d353b802d1651749bf0655a896b2b8b4ce99f
SonLam1122 <lamnthe181292@fpt.edu.vn>
2025-07-05 17:14:58 +0700

    update ui shipper dashboard

---
+++ Controllers/ShipperController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Village_Manager.Data;
+using Village_Manager.Models;
+
+namespace Village_Manager.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ShipperController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("dashboardshipper")]
+        public IActionResult DashboardShipper()
+        {
+            return View();
+        }
+        //public IActionResult OrdersShipper()
+        //{
+        //    return View();
+        //}
+        //public IActionResult DeliveriesShipper()
+        //{
+        //    return View();
+        //}
+        //public IActionResult ProfileShipper()
+        //{
+        //    return View();
+        //}
+        //public IActionResult HistoryShipper()
+        //{
+        //    return View();
+        //}
+        //public IActionResult NotificationsShipper()
+        //{
+        //    return View();
+        //}
+        //public IActionResult LogsShipper()
+        //{
+        //    return View();
+        //}
+    }
+}
