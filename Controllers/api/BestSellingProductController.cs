1fa9f340a44b742a0dbeaf8f994c5fe03b89af1f
hiep06092005 <hiepvtpt123@gmail.com>
2025-07-10 15:09:50 +0700

    BestSellingProduct-Retail API

---
+++ Controllers/api/BestSellingProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Village_Manager.Data;
+
+namespace Village_Manager.Controllers.api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BestSellingProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BestSellingProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("top-selling")]
+        public async Task<IActionResult> GetTopSellingProducts(int top = 5)
+        {
+            var result = await _context.RetailOrderItems
+                .Include(i => i.Product)
+                .GroupBy(i => new { i.ProductId, i.Product.Name, i.Product.Price, i.Product.Quantity })
+                .Select(g => new
+                {
+                    ProductId = g.Key.ProductId,
+                    Name = g.Key.Name,
+                    Price = g.Key.Price,
+                    Stock = g.Key.Quantity,
+                    TotalSold = g.Sum(x => x.Quantity),
+                    TotalRevenue = g.Sum(x => x.Quantity * x.UnitPrice),
+                    Image = _context.ProductImages
+                        .Where(p => p.ProductId == g.Key.ProductId)
+                        .Select(p => p.ImageUrl)
+                        .FirstOrDefault()
+                })
+                .OrderByDescending(x => x.TotalSold)
+                .Take(top)
+                .ToListAsync();
+
+            return Ok(result);
+        }
+    }
+}
